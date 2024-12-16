3dee8ab617b10282d14ebc2a8ab5735159106522
Daniil Aksonov <aksunov1@gmail.com>
2024-12-16 14:18:32 +0100

    Add disabled calendar cells before today cell

--- gloss/js/main.js
+++ gloss/js/main.js
@@ -1,15 +1,30 @@
-new AirDatepicker('.calendar', {
-    inline: true,
-    locale: {
-        days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-        daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        daysMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        today: 'Today',
-        clear: 'Clear',
-        dateFormat: 'MM/dd/yyyy',
-        timeFormat: 'hh:mm aa',
-        firstDay: 0
+let dp = new AirDatepicker('.calendar', {
+  inline: true,
+  locale: {
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Today',
+    clear: 'Clear',
+    dateFormat: 'MM/dd/yyyy',
+    timeFormat: 'hh:mm aa',
+    firstDay: 0
+  },
+  onRenderCell({date, cellType}) {
+    if (cellType === 'day') {
+      const now = new Date(Date.now());
+      let currentDate = new Date(Date.now());
+      currentDate.setHours(0, 0, 0, 0);
+      if ((date - currentDate == 0 && now.getHours() > 12 && now.getMinutes() > 30) || date < currentDate) {
+        return {
+          disabled: true
+        }
+      }
     }
-})
+  }
+});
+
+// Выбранная дата - dp.selectedDates[0]
+
