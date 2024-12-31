fa19cd427436f9711b3c0d9a2460c01ec6273d82
Daniil Aksonov <aksunov1@gmail.com>
2024-12-31 12:13:02 +0100

    feature: Add adaptive ✨

--- medi/js/main.js
+++ medi/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
-  $(".hero__slider").slick();
+  $(".hero__slider").slick({
+    responsive: [
+      {
+        breakpoint: 661,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+  $(".header__btn-menu").on("click", function() {
+    $(".header__btn-menu .btn-menu__line").toggleClass("btn-menu__line--active");
+    $(".header__list").toggleClass("header__list--active");
+    $("body").toggleClass("locked");
+  })
 });
