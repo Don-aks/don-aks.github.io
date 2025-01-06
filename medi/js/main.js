824cf7fe31c43f485ce8b5b23fff1f801274567e
Daniil Aksonov <aksunov1@gmail.com>
2025-01-06 17:55:13 +0100

    feat(medi): Add functional to tabs ✨

--- medi/js/main.js
+++ medi/js/main.js
@@ -13,5 +13,14 @@ $(function(){
     $(".header__btn-menu .btn-menu__line").toggleClass("btn-menu__line--active");
     $(".header__list").toggleClass("header__list--active");
     $("body").toggleClass("locked");
-  })
+  });
+
+  $(".tabs__tab").on("click", function(){
+    $(".tabs__tab").removeClass("tabs__tab--active");
+    $(this).addClass("tabs__tab--active");
+
+    let contentSelector = ".news__item[data-tab=" + $(this).data("tab") + "]";
+    $(".news__item").removeClass("news__item--active");
+    $(contentSelector).addClass("news__item--active");
+  });
 });
