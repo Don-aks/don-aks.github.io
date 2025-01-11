83aa96d2e333f47ec53fd454d3b6afe5df4fe8ea
Daniil Aksonov <aksunov1@gmail.com>
2025-01-11 21:25:24 +0100

    feat(souls): Add smooth scroll to header links ✨

--- souls/js/main.js
+++ souls/js/main.js
@@ -4,14 +4,20 @@ const hero = document.querySelector('.hero');
 closeBtn.addEventListener('click', function(){
   this.parentElement.style.display = 'none';
   header.style.height = '100vh';
-  hero.style.height = 'calc(100vh - 192px)'
+  hero.style.height = 'calc(100vh - 192px)';
 });
 
 const scrollBtn = document.querySelector('.scroll-down-btn');
-scrollBtn.addEventListener('click', function(e){
+scrollBtn.addEventListener('click', scrollIntoView);
+
   e.preventDefault();
-  const href = this.getAttribute('href').substring(1);
-  const scrollTarget = document.getElementById(href);
+  const href = e.target.getAttribute('href');
+  if (!href)
+    return;
+
+  const scrollTarget = document.getElementById(href.substring(1));
+  if (!scrollTarget)
+    return;
 
   const topOffset = 0;
   const elementPosition = scrollTarget.getBoundingClientRect().top;
@@ -21,4 +27,5 @@ scrollBtn.addEventListener('click', function(e){
       top: offsetPosition,
       behavior: 'smooth'
   });
-});
+}
+
