e0916b2b85d40b8edcc1bc20622c7febe53d681f
Daniil Aksonov <aksunov1@gmail.com>
2026-03-22 17:18:58 +0100

    🦜 feat: translate pages to Ukrainian

--- souls/js/lang.js
+++ souls/js/lang.js
@@ -1,326 +1,328 @@
 const langArray = {
-  "close-menu": {
-    "ru": "Закрыть меню",
-    "uk": "Закрити меню"
-  },
-  "open-menu": {
-    "ru": "Открыть меню",
-    "uk": "Відкрити меню"
-  },
-  "products": {
-    "ru": "Товары",
-    "uk": "Товари"
-  },
-  "who": {
-    "ru": "Кто мы?",
-    "uk": "Хто ми?"
-  },
-  "store": {
-    "ru": "Магазин",
-    "uk": "Магазин"
-  },
-  "recipes": {
-    "ru": "Рецепты",
-    "uk": "Рецепти"
-  },
-  "login": {
-    "ru": "Войти",
-    "uk": "Увійти"
-  },
-  "cart": {
-    "ru": "Корзина (0)",
-    "uk": "Кошик (0)"
-  },
-  "change-lng": {
-    "ru": "Сменить язык",
-    "uk": "Змінити мову"
-  },
-  "span1": {
-    "ru": "Попробуйте",
-    "uk": "Cкуштуйте"
-  },
-  "span2": {
-    "ru": "",
-    "uk": ""
-  },
-  "span3": {
-    "ru": "Дикое.",
-    "uk": "Дике."
-  },
-  "span4": {
-    "ru": "Найдите",
-    "uk": "Знайдіть"
-  },
-  "span5": {
-    "ru": "Свою",
-    "uk": "Свою"
-  },
-  "span6": {
-    "ru": "Душу.",
-    "uk": "Душу."
-  },
-  "use-cookies": {
-    "ru": "Наш сайт использует файлы",
-    "uk": "Наш веб-сайт використовує файли"
-  },
-  "products-title": {
-    "ru": "Дико любимое",
-    "uk": "Дико улюблене"
-  },
-  "products-text": {
-    "ru": "Вы найдете наши дикие продукты в их самой простой форме. Никакие добавки или что-либо еще не могут испортить их природу.",
-    "uk": "Ви знайдете наші дикі продукти в їхній найпростішій формі. Ніякі добавки або будь-що ще не можуть зіпсувати їх природу."
-  },
-  "product1": {
-    "ru": "Масло из Дикого Арахиса",
-    "uk": "Масло Дикого Арахісу"
-  },
-  "product2": {
-    "ru": "Тахини с Какао и Мёдом",
-    "uk": "Тахіні з Какао та Медом"
-  },
-  "product3": {
-    "ru": "Масло из Дикого Лесного Ореха",
-    "uk": "Масло з Дикого Лісового Горіха"
-  },
-  "product4": {
-    "ru": "Дикий Тахини",
-    "uk": "Дикий Тахіні"
-  },
-  "wild-way": {
-    "ru": "Наш дикий путь",
-    "uk": "Наш дикий шлях"
-  },
-  "photos-text": {
-    "ru": "Наша философия присутствует на каждом этапе нашего производства. Мы стремимся внести свой вклад в создание лучшего мира.",
-    "uk": "Наша філософія є на кожному етапі нашого виробництва. Ми прагнемо зробити свій внесок у створення кращого світу."
-  },
-  "photos-img1": {
-    "ru": "Товары Wild Souls",
-    "uk": "Товари Wild Souls"
-  },
-  "photos-img2": {
-    "ru": "Напитки Wild Souls",
-    "uk": "Напої Wild Souls"
-  },
-  "photos-img3": {
-    "ru": "Рука, открывающая водопроводный кран",
-    "uk": "Рука відкриває водопровідний кран"
-  },
-  "discover": {
-    "ru": "УЗНАТЬ БОЛЬШЕ",
-    "uk": "ДІЗНАТИСЯ БІЛЬШЕ"
-  },
-  "nut-title": {
-    "ru": "Дикий как орех??",
-    "uk": "Дикий як горіх??"
-  },
-  "nut-card-title": {
-    "ru": "Жареный миндаль",
-    "uk": "Смажений мигдаль"
-  },
-  "nut-text": {
-    "ru": "Все сокровища спрятаны в сундуке. Соответственно, наши сокровища заключены в свою Дикую оболочку.",
-    "uk": "Всі скарби заховані в скрині. Відповідно, наші скарби укладені у свою Дику оболонку."
-  },
-  "nut-price": {
-    "ru": "250г | 220 <span class=\"nut-product__currency\">₴</span>",
-    "uk": "250г | 220 <span class=\"nut-product__currency\">₴</span>"
-  },
-  "nut-link": {
-    "ru": "ПОПРОБОВАТЬ!",
-    "uk": "СКУШТУВАТИ!"
-  },
-  "discover-title": {
-    "ru": "ОТКРОЙТЕ ДЛЯ СЕБЯ НАШУ ПРОДУКЦИЮ ЧЕРЕЗ 5 СОКРОВИЩ ПРИРОДЫ!",
-    "uk": "ВІДКРИЙТЕ ДЛЯ НАШІ ПРОДУКТИ ЧЕРЕЗ 5 СКАРБІВ ПРИРОДИ!"
-  },
-  "sesame": {
-    "ru": "Кунжут",
-    "uk": "Кунжут"
-  },
-  "peanut": {
-    "ru": "Арахис",
-    "uk": "Арахіс"
-  },
-  "almond": {
-    "ru": "Миндаль",
-    "uk": "Мигдаль"
-  },
-  "hazelnut": {
-    "ru": "Фундук",
-    "uk": "Фундук"
-  },
-  "cashew": {
-    "ru": "Кешью",
-    "uk": "Кеш'ю"
-  },
-  "slider-title": {
-    "ru": "Слова от души!",
-    "uk": "Слова від душі!"
-  },
-  "slider-title1": {
-    "ru": "ПОПАГАНДА",
-    "uk": "ПОПАГАНДА"
-  },
-  "slider-text1": {
-    "ru": "Другой магазин в центре Афин измельчает фрукты, предлагает масло, приготовленное всего из одного ингредиента, и вкусы халвы, которые подтверждают, что полезное может быть вкусным.",
-    "uk": "В іншому магазині в центрі Афін подрібнюють фрукти, пропонують масло, виготовлене лише з одного інгредієнта, і ароматизатори халви, які підтверджують, що здорове може бути смачним."
-  },
-  "slider-title2": {
-    "ru": "АФИНСКИЙ ГОЛОС",
-    "uk": "АФІНСЬКИЙ ГОЛОС"
-  },
-  "slider-text2": {
-    "ru": "Wild Souls работает в основном с кунжутом и ореховыми продуктами (фисташками, фундуком, миндалем, кешью), создавая адские пасты, которые хочется глотать вместе с банкой.",
-    "uk": "Wild Souls працює переважно з продуктами з кунжуту та горіхів (фісташками, фундуком, мигдалем, кеш’ю), створюючи пекельні спреди, які хочеться проковтнути разом із баночкою."
-  },
-  "store-title": {
-    "ru": "Концептуальный магазин",
-    "uk": "Концептуальний магазин"
-  },
-  "store-text": {
-    "ru": "Приглашаем вас исследовать его, жить им и фотографировать его. Но будьте осторожны, это очень затягивает!",
-    "uk": "Ми запрошуємо вас дослідити його, прожити та сфотографувати. Але будьте обережні, це викликає сильну залежність!"
-  },
-  "halva-title": {
-    "ru": "Дикая Халва",
-    "uk": "Дика Халва"
-  },
-  "halva-subtitle": {
-    "ru": "Волшебное семечко..",
-    "uk": "Чарівне зернятко.."
-  },
-  "halva-text": {
-    "ru": "Это не десерт, не закуска и не полноценная еда. Но это все одновременно. Это то, чем вы хотите, чтобы оно было.",
-    "uk": "Це не десерт, не перекус і не повноцінна їжа. Але все це одночасно. Це те, що ви хочете бачити."
-  },
-  "halva-price": {
-    "ru": "500г | ВІД 399<span class=\"halva__currency\">₴</span>",
-    "uk": "500г | ОТ 399<span class=\"halva__currency\">₴</span>"
-  },
-  "discover-now": {
-    "ru": "ОТКРОЙТЕ СЕЙЧАС!",
-    "uk": "ВІДКРИЙТЕ ЗАРАЗ!"
-  },
-  "gift-title": {
-    "ru": "Поделитесь своей Любовью с помощью Дикого Подарка!",
-    "uk": "Поширюйте свою Любов за допомогою Дикого Подарунка!"
-  },
-  "five-treasures": {
-    "ru": "Пять сокровищ",
-    "uk": "П'ять скарбів"
-  },
-  "treasures-text": {
-    "ru": "Как сказал Гиппократ, мы — то, что мы едим.<br> Давайте посмотрим, что произойдет, если съесть эти ДИКИЕ сокровища.",
-    "uk": "Як казав Гіппократ, ми — це те, що ми їмо.<br> Давайте подивимось, що станеться, якщо з’їсти ці ДИКІ скарби."
-  },
-  "recipes-title": {
-    "ru": "Дикие Рецепты",
-    "uk": "Дикі Рецепти"
-  },
-  "recipes-text": {
-    "ru": "Загляните в замочную скважину нашей Дикой кухни! Получите идеи, как превратить свою кухню в Дикую.",
-    "uk": "Подивіться на замкову щілину нашої Дикої кухні! Отримайте ідеї про те, як перетворити власну кухню на Дику."
-  },
-  "sweet": {
-    "ru": "Сладкое",
-    "uk": "Солодке"
-  },
-  "granola": {
-    "ru": "Дикая Гранола из 6 Ингредиентов",
-    "uk": "Дика гранола з 6 інгредієнтів"
-  },
-  "salty": {
-    "ru": "Солёное",
-    "uk": "Солоне"
-  },
-  "coleslaw": {
-    "ru": "Салат из капусты с тахини, богатым клетчаткой",
-    "uk": "Салат з капусти та тахіні з високим вмістом клітковини"
-  },
-  "hummus": {
-    "ru": "Дикий Хумус",
-    "uk": "Дикий Хумус"
-  },
-  "footer-title": {
-    "ru": "Оставайся Диким",
-    "uk": "Залишайся Диким"
-  },
-  "input": {
-    "ru": "Напишите свой email",
-    "uk": "Напишіть свій email"
-  },
-  "follow": {
-    "ru": "Подпишитесь",
-    "uk": "Слідкуйте за Нами"
-  },
-  "website": {
-    "ru": "Сайт создан",
-    "uk": "Сайт створений"
-  },
-  "butters": {
-    "ru": "Ореховые пасты",
-    "uk": "Горіхове масло"
-  },
-  "honey": {
-    "ru": "Мёд",
-    "uk": "Мед"
-  },
-  "marmalades": {
-    "ru": "Мармелады",
-    "uk": "Мармелади"
-  },
-  "halva": {
-    "ru": "Халва",
-    "uk": "Халва"
-  },
-  "dry-nuts": {
-    "ru": "Сухие Орехи",
-    "uk": "Сухі Горіхи"
-  },
-  "open-sesame": {
-    "ru": "Открытый кунжут",
-    "uk": "Відкрий кунжут"
-  },
-  "card": {
-    "ru": "Подарочная карта",
-    "uk": "Подарункова карта"
-  },
-  "footer-title2": {
-    "ru": "Контакты",
-    "uk": "Контакти"
-  },
-  "footer-title3": {
-    "ru": "Компания",
-    "uk": "Компанія"
-  },
-  "concept-store": {
-    "ru": "Концептуальный Магазин",
-    "uk": "Концептуальний Магазин"
-  },
-  "help": {
-    "ru": "Помощь",
-    "uk": "Допомога"
-  },
-  "terms": {
-    "ru": "Условия и Положения",
-    "uk": "Правила та Умови"
-  },
-  "policy": {
-    "ru": "Политика Конфиденциальности",
-    "uk": "Політика Конфіденційності"
-  },
-  "cookies": {
-    "ru": "Файлы Cookie",
-    "uk": "Файли Cookie"
-  },
-  "methods": {
-    "ru": "Способы Оплаты",
-    "uk": "Способи Оплати"
-  },
-  "shipping": {
-    "ru": "Доставка и Возврат",
-    "uk": "Доставка та Повернення"
-  }
-}
+  'close-menu': {
+    pl: 'Zamknij menu',
+    uk: 'Закрити меню',
+  },
+  'open-menu': {
+    pl: 'Otwórz menu',
+    uk: 'Відкрити меню',
+  },
+  products: {
+    pl: 'Produkty',
+    uk: 'Товари',
+  },
+  who: {
+    pl: 'Kim jesteśmy?',
+    uk: 'Хто ми?',
+  },
+  store: {
+    pl: 'Sklep',
+    uk: 'Магазин',
+  },
+  recipes: {
+    pl: 'Przepisy',
+    uk: 'Рецепти',
+  },
+  login: {
+    pl: 'Zaloguj się',
+    uk: 'Увійти',
+  },
+  cart: {
+    pl: 'Koszyk (0)',
+    uk: 'Кошик (0)',
+  },
+  'change-lng': {
+    pl: 'Zmień język',
+    uk: 'Змінити мову',
+  },
+  span1: {
+    pl: 'Spróbuj',
+    uk: 'Cкуштуйте',
+  },
+  span2: {
+    pl: '',
+    uk: '',
+  },
+  span3: {
+    pl: 'Dzikie.',
+    uk: 'Дике.',
+  },
+  span4: {
+    pl: 'Znajdź',
+    uk: 'Знайдіть',
+  },
+  span5: {
+    pl: 'Swoją',
+    uk: 'Свою',
+  },
+  span6: {
+    pl: 'Duszę.',
+    uk: 'Душу.',
+  },
+  'use-cookies': {
+    pl: 'Nasza strona używa plików cookie',
+    uk: 'Наш веб-сайт використовує файли',
+  },
+  'products-title': {
+    pl: 'Dziko ukochane',
+    uk: 'Дико улюблене',
+  },
+  'products-text': {
+    pl: 'Znajdziesz nasze dzikie produkty w ich najprostszej formie. Żadne dodatki ani nic innego nie mogą zepsuć ich natury.',
+    uk: 'Ви знайдете наші дикі продукти в їхній найпростішій формі. Ніякі добавки або будь-що ще не можуть зіпсувати їх природу.',
+  },
+  product1: {
+    pl: 'Masło z Dziki Arachidu',
+    uk: 'Масло Дикого Арахісу',
+  },
+  product2: {
+    pl: 'Tahini z Kakao i Miodem',
+    uk: 'Тахіні з Какао та Медом',
+  },
+  product3: {
+    pl: 'Masło z Dziki Orzech Leśny',
+    uk: 'Масло з Дикого Лісового Горіха',
+  },
+  product4: {
+    pl: 'Dziki Tahini',
+    uk: 'Дикий Тахіні',
+  },
+  'wild-way': {
+    pl: 'Nasza dzika droga',
+    uk: 'Наш дикий шлях',
+  },
+  'photos-text': {
+    pl: 'Nasza filozofia jest obecna na każdym etapie naszej produkcji. Staramy się przyczynić do stworzenia lepszego świata.',
+    uk: 'Наша філософія є на кожному етапі нашого виробництва. Ми прагнемо зробити свій внесок у створення кращого світу.',
+  },
+  'photos-img1': {
+    pl: 'Produkty Wild Souls',
+    uk: 'Товари Wild Souls',
+  },
+  'photos-img2': {
+    pl: 'Napoje Wild Souls',
+    uk: 'Напої Wild Souls',
+  },
+  'photos-img3': {
+    pl: 'Ręka otwierająca kran wodociągowy',
+    uk: 'Рука відкриває водопровідний кран',
+  },
+  discover: {
+    pl: 'Dowiedz się więcej',
+    uk: 'ДІЗНАТИСЯ БІЛЬШЕ',
+  },
+  'nut-title': {
+    pl: 'Dziki jak orzech??',
+    uk: 'Дикий як горіх??',
+  },
+  'nut-card-title': {
+    pl: 'Prażony migdał',
+    uk: 'Смажений мигдаль',
+  },
+  'nut-text': {
+    pl: 'Wszystkie skarby są ukryte w skrzyni. Odpowiednio, nasze skarby są zamknięte w swojej Dzikiej powłoce.',
+    uk: 'Всі скарби заховані в скрині. Відповідно, наші скарби укладені у свою Дику оболонку.',
+  },
+  'nut-price': {
+    pl: '250g | 20 <span class="nut-product__currency">zł</span>',
+    uk: '250г | 220 <span class="nut-product__currency">₴</span>',
+  },
+  'nut-link': {
+    pl: 'SPRÓBUJ!',
+    uk: 'СКУШТУВАТИ!',
+  },
+  'discover-title': {
+    pl: 'ODKRYJ NASZĄ PRODUKCJĘ PRZEZ 5 SKARBÓW NATURY!',
+    uk: 'ВІДКРИЙТЕ ДЛЯ НАШІ ПРОДУКТИ ЧЕРЕЗ 5 СКАРБІВ ПРИРОДИ!',
+  },
+  sesame: {
+    pl: 'Kozieradka',
+    uk: 'Кунжут',
+  },
+  peanut: {
+    pl: 'Orzech ziemny',
+    uk: 'Арахіс',
+  },
+  almond: {
+    pl: 'Migdał',
+    uk: 'Мигдаль',
+  },
+  hazelnut: {
+    pl: 'Orzech laskowy',
+    uk: 'Фундук',
+  },
+  cashew: {
+    pl: 'Orzech nerkowca',
+    uk: "Кеш'ю",
+  },
+  'slider-title': {
+    pl: 'Słowa od duszy!',
+    uk: 'Слова від душі!',
+  },
+  'slider-title1': {
+    pl: 'PROPAGANDA',
+    uk: 'ПОПАГАНДА',
+  },
+  'slider-text1': {
+    pl: 'Inny sklep w centrum Aten rozdrabnia owoce, oferuje masło przygotowane z zaledwie jednego składnika oraz smaki chałwy, które potwierdzają, że zdrowe może być smaczne.',
+    uk: 'В іншому магазині в центрі Афін подрібнюють фрукти, пропонують масло, виготовлене лише з одного інгредієнта, і ароматизатори халви, які підтверджують, що здорове може бути смачним.',
+  },
+  'slider-title2': {
+    pl: 'GŁOS Z ATEN',
+    uk: 'АФІНСЬКИЙ ГОЛОС',
+  },
+  'slider-text2': {
+    pl: 'Wild Souls pracuje głównie z produktami z sezamu i orzechów (pistacjami, orzechami laskowymi, migdałami, nerkowcami), tworząc piekielne pasty, które chce się połykać razem ze słoikiem.',
+    uk: 'Wild Souls працює переважно з продуктами з кунжуту та горіхів (фісташками, фундуком, мигдалем, кеш’ю), створюючи пекельні спреди, які хочеться проковтнути разом із баночкою.',
+  },
+  'store-title': {
+    pl: 'Sklep koncepcyjny',
+    uk: 'Концептуальний магазин',
+  },
+  'store-text': {
+    pl: 'Zapraszamy do odkrywania, życia i fotografowania. Ale uważaj, to bardzo wciąga!',
+    uk: 'Ми запрошуємо вас дослідити його, прожити та сфотографувати. Але будьте обережні, це викликає сильну залежність!',
+  },
+  'halva-title': {
+    pl: 'Dzika chałwa',
+    uk: 'Дика Халва',
+  },
+  'halva-subtitle': {
+    pl: 'Magiczne ziarenko..',
+    uk: 'Чарівне зернятко..',
+  },
+  'halva-text': {
+    pl: 'To nie deser, nie przekąska i nie pełnowartościowe jedzenie. Ale to wszystko naraz. To to, czym chcesz, żeby było.',
+    uk: 'Це не десерт, не перекус і не повноцінна їжа. Але все це одночасно. Це те, що ви хочете бачити.',
+  },
+  'halva-price': {
+    pl: '500g | OD 35 <span class="halva__currency">zł</span>',
+    uk: '500г | ОТ 399<span class="halva__currency">₴</span>',
+  },
+  'discover-now': {
+    pl: 'ODKRYJ TERAZ!',
+    uk: 'ВІДКРИЙТЕ ЗАРАЗ!',
+  },
+  'gift-title': {
+    pl: 'Podziel się swoją Miłością za pomocą Dzikiego Prezentu!',
+    uk: 'Поширюйте свою Любов за допомогою Дикого Подарунка!',
+  },
+  'five-treasures': {
+    pl: 'Pięć skarbów',
+    uk: "П'ять скарбів",
+  },
+  'treasures-text': {
+    pl: 'Jak powiedział Hipokrates, jesteśmy tym, co jemy.<br> Zobaczmy, co się stanie, jeśli zjemy te DZIKIE skarby.',
+    uk: 'Як казав Гіппократ, ми — це те, що ми їмо.<br> Давайте подивимось, що станеться, якщо з’їсти ці ДИКІ скарби.',
+  },
+  'recipes-title': {
+    pl: 'Dzikie Przepisy',
+    uk: 'Дикі Рецепти',
+  },
+  'recipes-text': {
+    pl: 'Zajrzyj przez dziurkę od klucza do naszej Dzikej kuchni! Zdobądź pomysły, jak przemienić swoją kuchnię w Dziką.',
+    uk: 'Подивіться на замкову щілину нашої Дикої кухні! Отримайте ідеї про те, як перетворити власну кухню на Дику.',
+  },
+  sweet: {
+    pl: 'Słodkie',
+    uk: 'Солодке',
+  },
+  granola: {
+    pl: 'Dzika Granola z 6 Składników',
+    uk: 'Дика гранола з 6 інгредієнтів',
+  },
+  salty: {
+    pl: 'Słone',
+    uk: 'Солоне',
+  },
+  coleslaw: {
+    pl: 'Sałatka z kapusty z tahini, bogata w błonnik',
+    uk: 'Салат з капусти та тахіні з високим вмістом клітковини',
+  },
+  hummus: {
+    pl: 'Dziki Hummus',
+    uk: 'Дикий Хумус',
+  },
+  'footer-title': {
+    pl: 'Pozostań Dziki',
+    uk: 'Залишайся Диким',
+  },
+  input: {
+    pl: 'Napisz swój email',
+    uk: 'Напишіть свій email',
+  },
+  follow: {
+    pl: 'Obserwuj nas',
+    uk: 'Слідкуйте за Нами',
+  },
+  website: {
+    pl: 'Strona stworzona',
+    uk: 'Сайт створений',
+  },
+  butters: {
+    pl: 'Masła orzechowe',
+    uk: 'Горіхове масло',
+  },
+  honey: {
+    pl: 'Miód',
+    uk: 'Мед',
+  },
+  marmalades: {
+    pl: 'Marmolady',
+    uk: 'Мармелади',
+  },
+  halva: {
+    pl: 'Chałwa',
+    uk: 'Халва',
+  },
+  'dry-nuts': {
+    pl: 'Suszone Orzechy',
+    uk: 'Сухі Горіхи',
+  },
+  'open-sesame': {
+    pl: 'Otwórz sezam',
+    uk: 'Відкрий кунжут',
+  },
+  card: {
+    pl: 'Karta podarunkowa',
+    uk: 'Подарункова карта',
+  },
+  'footer-title2': {
+    pl: 'Kontakt',
+    uk: 'Контакти',
+  },
+  'footer-title3': {
+    pl: 'Firma',
+    uk: 'Компанія',
+  },
+  'concept-store': {
+    pl: 'Sklep Koncepcyjny',
+    uk: 'Концептуальний Магазин',
+  },
+  help: {
+    pl: 'Pomoc',
+    uk: 'Допомога',
+  },
+  terms: {
+    pl: 'Warunki i Zasady',
+    uk: 'Правила та Умови',
+  },
+  policy: {
+    pl: 'Polityka Prywatności',
+    uk: 'Політика Конфіденційності',
+  },
+  cookies: {
+    pl: 'Pliki Cookie',
+    uk: 'Файли Cookie',
+  },
+  methods: {
+    pl: 'Metody Płatności',
+    uk: 'Способи Оплати',
+  },
+  shipping: {
+    pl: 'Dostawa i Zwroty',
+    uk: 'Доставка та Повернення',
+  },
+};
+
+export default langArray;
