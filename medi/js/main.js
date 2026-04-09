e510110537725c9b4b62819e581e98c93599851f
Daniil Aksonov <aksunov1@gmail.com>
2026-04-09 13:31:58 +0200

    ♻️ refactor(medi/js): move all elements to UI object

--- medi/js/main.js
+++ medi/js/main.js
@@ -6,58 +6,77 @@ var HIDDEN_CELLS =
   '.header__cell-phone, .header__cell-salons, .header__cell-social';
 
 var $window = $(window);
-var $header;
 var $body = $(document.body);
 
-var $btnMenuLine;
-var $headerList;
-var $menuFocusableElements;
-var $hiddenCells;
-var $hiddenCellsFocusableElements;
+var $header = $('.header__top');
+var $headerBtnMenu = $header.find('.header__btn-menu');
+var $tabs = $('.tabs__tab');
 
-$('.skip-to-content').on('click', function () {
-  $('#main').trigger('focus');
+var UI = {
+  $skipToContent: $('.skip-to-content'),
+  $content: $('#main'),
+
+  $highContrastBtn: $('.btn--visually-impaired'),
+
+  $header: $header,
+  $headerList: $header.find('.header__list'),
+
+  $headerBtnMenu: $headerBtnMenu,
+  $btnMenuLine: $headerBtnMenu.find('.btn-menu__line'),
+  $hiddenCells: $header.find(HIDDEN_CELLS),
+
+  $btnBooking: $('.header__btn-booking'),
+
+  $salonsSubmenu: $('.salons__submenu'),
+  $salonsLinks: $('.salons__link'),
+  salonsHideClass: 'salons__submenu--hidden',
+
+  $heroSlider: $('.hero__slider'),
+  sliderBtnPrevHTML:
+    '<button class="slick-prev" aria-label="Попередній слайд" type="button">←</button>',
+  sliderBtnNextHTML:
+    '<button class="slick-next" aria-label="Наступний слайд" type="button">→</button>',
+
+  $tabs: $tabs,
+  $tabsBtns: $tabs.find('.tabs__btn'),
+
+  $modalForm: $('.modal__form'),
+  modalThanksSelector: '#modal-thanks',
+};
+
+UI.$skipToContent.on('click', function () {
+  UI.$content.trigger('focus');
 });
 
-$header = $('.header__top');
 $window.on('scroll', throttle(handleScroll, 150));
 
-$('.btn--visually-impaired').on('click', function () {
+UI.$highContrastBtn.on('click', function () {
   var isActive = $body.hasClass('visually-impaired');
   $body.toggleClass('visually-impaired', !isActive);
 });
 
-var $headerBtnMenu = $('.header__btn-menu');
-$btnMenuLine = $headerBtnMenu.find('.btn-menu__line');
-
-$headerList = $('.header__list');
-$menuFocusableElements = $headerList.find(FOCUSABLE_SELECTORS);
-
-var $hiddenCells = $header.find(HIDDEN_CELLS);
-$hiddenCellsFocusableElements = $hiddenCells.find(FOCUSABLE_SELECTORS);
-
 if ($window.outerWidth() <= HIDE_MENU_BREAKPOINT) {
-  setTabIndex($menuFocusableElements, '-1');
-  setTabIndex($hiddenCellsFocusableElements, '-1');
+  setTabIndex(getFocusable(UI.$headerList), '-1');
+  setTabIndex(getFocusable(UI.$hiddenCells), '-1');
 }
 
-$headerBtnMenu.on('click', handleBtnMenuClick);
+UI.$headerBtnMenu.on('click', handleBtnMenuClick);
 $window.on('resize', debounce(handleResize, 150));
 $body.on('click', handleSalonsClick);
 
-pulse($('.header__btn-booking'));
+pulse(UI.$btnBooking);
 
-$('.modal__form').on('submit', function (event) {
+UI.$modalForm.on('submit', function (event) {
   event.preventDefault();
 
   $.fancybox.close();
   $.fancybox.open({
-    src: '#modal-thanks',
+    src: UI.modalThanksSelector,
     type: 'inline',
   });
 });
 
-$('.hero__slider').slick({
+UI.$heroSlider.slick({
   responsive: [
     {
       breakpoint: 1331,
@@ -69,74 +88,73 @@ $('.hero__slider').slick({
   autoplay: true,
   autoplaySpeed: 5000,
 
-  prevArrow:
-    '<button class="slick-prev" aria-label="Попередній слайд" type="button">←</button>',
-  nextArrow:
-    '<button class="slick-next" aria-label="Наступний слайд" type="button">→</button>',
+  prevArrow: UI.sliderBtnPrevHTML,
+  nextArrow: UI.sliderBtnNextHTML,
 });
 
-var $heroSliderWrap = $('.hero__slider-wrap');
-$heroSliderWrap.on('mousedown', function (e) {
-  $heroSliderWrap.css('cursor', 'grabbing');
+UI.$heroSlider.on('mousedown', function (e) {
+  UI.$heroSlider.css('cursor', 'grabbing');
 });
 
-$heroSliderWrap.on('mouseup', function (e) {
-  $heroSliderWrap.css('cursor', '');
+UI.$heroSlider.on('mouseup', function (e) {
+  UI.$heroSlider.css('cursor', '');
 });
 
-var $tabs = $('.tabs__tab');
-var $tabsBtns = $tabs.find('.tabs__btn');
-
-$tabsBtns.on('click', { $tabs: $tabs, $tabsBtns: $tabsBtns }, toggleTab);
+UI.$tabsBtns.on(
+  'click',
+  { $tabs: UI.$tabs, $tabsBtns: UI.$tabsBtns },
+  toggleTab,
+);
 
 // FUNCTIONS
 
 function handleScroll() {
   if ($window.scrollTop() > 0) {
-    $header.addClass('header__top--scrolled');
+    UI.$header.addClass('header__top--scrolled');
     return;
   }
 
-  $header.removeClass('header__top--scrolled');
+  UI.$header.removeClass('header__top--scrolled');
 }
 
 function handleBtnMenuClick() {
-  var isActive = $headerList.hasClass('header__list--active');
+  var isActive = UI.$headerList.hasClass('header__list--active');
 
-  $btnMenuLine.toggleClass('btn-menu__line--active', !isActive);
-  $headerList.toggleClass('header__list--active', !isActive);
+  UI.$btnMenuLine.toggleClass('btn-menu__line--active', !isActive);
+  UI.$headerList.toggleClass('header__list--active', !isActive);
   $body.toggleClass('locked', !isActive);
 
-  setTabIndex($menuFocusableElements, !isActive ? '0' : '-1');
-  setTabIndex($hiddenCellsFocusableElements, !isActive ? '0' : '-1');
+  setTabIndex(getFocusable(UI.$headerList), !isActive ? '0' : '-1');
+  setTabIndex(getFocusable(UI.$hiddenCells), !isActive ? '0' : '-1');
 }
 
 function handleResize() {
+  var $headerListFocusableElements = getFocusable(UI.$headerList);
+  var $hiddenCellsFocusableElements = getFocusable(UI.$hiddenCells);
+
   if ($window.outerWidth() > HIDE_MENU_BREAKPOINT) {
-    setTabIndex($menuFocusableElements, '0');
+    setTabIndex($headerListFocusableElements, '0');
     setTabIndex($hiddenCellsFocusableElements, '0');
     return;
   }
 
-  setTabIndex($menuFocusableElements, '-1');
+  setTabIndex($headerListFocusableElements, '-1');
   setTabIndex($hiddenCellsFocusableElements, '-1');
 }
 
 function handleSalonsClick(e) {
-  var $salonsSubmenu = $('.salons__submenu');
-
-  var className = 'salons__submenu--hidden';
+  var className = '';
   var $target = $(e.target);
 
   var isToggleBtn = $target.closest('.salons').length;
   var isSubmenu = $target.closest('.salons__submenu').length;
 
-  if (isToggleBtn && $salonsSubmenu.hasClass(className)) {
+  if (isToggleBtn && UI.$salonsSubmenu.hasClass(UI.salonsHideClass)) {
     showSalonsSubmenu();
     return;
   }
 
-  if (isToggleBtn && !$salonsSubmenu.hasClass(className)) {
+  if (isToggleBtn && !UI.$salonsSubmenu.hasClass(UI.salonsHideClass)) {
     hideSalonsSubmenu();
     return;
   }
@@ -148,21 +166,13 @@ function handleSalonsClick(e) {
 }
 
 function showSalonsSubmenu() {
-  var className = 'salons__submenu--hidden';
-  var $salonsSubmenu = $('.salons__submenu');
-  var $salonsLinks = $('.salons__link');
-
-  $salonsSubmenu.removeClass(className);
-  setTabIndex($salonsLinks, '0');
+  UI.$salonsSubmenu.removeClass(UI.salonsHideClass);
+  setTabIndex(UI.$salonsLinks, '0');
 }
 
 function hideSalonsSubmenu() {
-  var className = 'salons__submenu--hidden';
-  var $salonsSubmenu = $('.salons__submenu');
-  var $salonsLinks = $('.salons__link');
-
-  $salonsSubmenu.addClass(className);
-  setTabIndex($salonsLinks, '-1');
+  UI.$salonsSubmenu.addClass(UI.salonsHideClass);
+  setTabIndex(UI.$salonsLinks, '-1');
 }
 
 function pulse($element, delay = 5000) {
@@ -176,15 +186,14 @@ function pulse($element, delay = 5000) {
   }, 500);
 }
 
-function toggleTab(event) {
+function toggleTab() {
   var $container = $(this).closest('.tabs__tab');
-  var $tabs = event.data.$tabs;
   var className = 'tabs__tab--active';
 
-  $tabs.removeClass(className);
+  UI.$tabs.removeClass(className);
   $container.addClass(className);
 
-  var $tabsBtns = $tabs.find('.tabs__btn');
+  var $tabsBtns = UI.$tabs.find('.tabs__btn');
   setTabIndex($tabsBtns, '0');
 
   var $btn = $container.find('.tabs__btn');
@@ -240,4 +249,8 @@ function setTabIndex($elements, value) {
     if ($(this).attr('tabindex') === value) return;
     $(this).attr('tabindex', value);
   });
+}
+
+function getFocusable($container) {
+  return $container.find(FOCUSABLE_SELECTORS);
 }
