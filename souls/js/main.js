bbd327d4924bf24c0ece2d860e7ab4a880278cae
Daniil Aksonov <aksunov1@gmail.com>
2026-05-08 12:30:39 +0200

    ♻️ refactor(souls/menu): simplify toggle logic

--- souls/js/main.js
+++ souls/js/main.js
@@ -302,9 +302,8 @@ function closeNotify() {
 }
 
 function changeMenuState() {
-  const isActive = menu.classList.contains('header__menu--active');
-  menu.classList.toggle('header__menu--active', !isActive);
-  body.classList.toggle('locked', !isActive);
+  const willBeActive = menu.classList.toggle('header__menu--active');
+  document.documentElement.classList.toggle(lockedClass, willBeActive);
 
   const links = document.querySelectorAll('.header__menu .menu__link');
 
